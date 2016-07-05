b7817a433fb2c8ab6658bd3498f15c3f2bd236e3
Craig Day <cday@zendesk.com>
2016-07-05 14:13:04 +1000

    update the method signature, specs, and docs for the numbers module

--- lib/cldr/numbers.ex
+++ lib/cldr/numbers.ex
@@ -1,15 +1,39 @@
 defmodule CLDRex.Numbers do
   @moduledoc """
-
+  Provide number localization including conversion to currency, percentage, and
+  decimal values.
   """
+  @type locale :: atom | String.t
 
-  @spec localize_number(number, atom) :: String.t
-  def localize_number(number, locale) do
+  @doc """
+  Localize the given number into the given locale.
+  """
+  @spec localize(number, locale) :: String.t
+  def localize(number, locale) do
 
   end
 
-  # @spec localize_number(number, atom, %{...}) :: String.t
-  def localize_number(number, locale, options) do
+  @doc """
+  Convert the given number to the localized currency format.
+  """
+  @spec to_currency(number, locale) :: String.t
+  def to_currency(number, locale) do
+
+  end
+
+  @doc """
+  Convert the given number to the localized percentage format.
+  """
+  @spec to_percent(number, locale) :: String.t
+  def to_percent(number, locale) do
+
+  end
+
+  @doc """
+  Convert the given number to the localized decimal format.
+  """
+  @spec to_decimal(number, locale) :: String.t
+  def to_decimal(number, locale) do
 
   end
 end
